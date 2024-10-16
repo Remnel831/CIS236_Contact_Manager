b9582c3bfd4092f1a27a0af00cd1325a8f494be3
Remnel831 <136384272+Remnel831@users.noreply.github.com>
2024-10-16 16:13:16 -0500

    Add project files.

---
+++ CIS236_Contact_Manager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CIS236_Contact_Manager.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
