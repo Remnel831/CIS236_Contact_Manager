f61ce37cf399629459a0833d500f377bd1465984
Victoria <150954681+nxpeach@users.noreply.github.com>
2024-10-19 13:51:20 -0500

    Edited controller

--- CIS236_Contact_Manager/Controllers/ContactController.cs
+++ CIS236_Contact_Manager/Controllers/ContactController.cs
@@ -13,13 +13,65 @@ namespace CIS236_Contact_Manager.Controllers
 			context = ctx;
 		}
 
-        [HttpGet]
-        public IActionResult Details(int id)
-        {
-            var contact = context.Contacts.Find(id);
+		[HttpGet]
+		public IActionResult Details(int id)
+		{
+			var contact = context.Contacts.Find(id);
 			ViewBag.Category = context.Categories.OrderBy(c => c.Name).ToList();
-            return View(contact);
-        }
+			return View(contact);
+		}
 
-    }
+		[HttpGet]
+		public IActionResult Delete(int id)
+		{
+			var contact = context.Contacts.Find(id);
+			return View(contact);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(Contact contact)
+		{
+			context.Contacts.Remove(contact);
+			context.SaveChanges();
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			ViewBag.Action = "Edit";
+			ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+			var contact = context.Contacts.Find(id);
+			return View(contact);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Contact contact)
+		{
+			if (ModelState.IsValid)
+			{
+				if (contact.ContactId == 0)
+				{
+					context.Contacts.Add(contact);
+				}
+
+				else
+				{
+					context.Contacts.Update(contact);
+
+				}
+
+				context.SaveChanges();
+				return View("Index");
+			}
+
+			else
+			{
+				ViewBag.Action = (contact.ContactId == 0) ? "Add" : "Edit";
+				ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+				return View(contact);
+			}
+		}
+	}
 }
+
